2f9766e0f17eadaabe11bbd49f8ccc45432ec735
aman-anand-X-hub <aman.anand@xarterian.com>
2024-05-31 16:45:45 +0530

    chap-16 done

--- Chapter16(Project)/PausingTheGame/game.js
+++ Chapter16(Project)/PausingTheGame/game.js
@@ -333,9 +333,7 @@ function runLevel(level, Display) {
   });
 }
 
-// TODO
 async function runGame(plans, Display) {
-  // Your code here
   let lives = 3;
   for (let level = 0; level < plans.length && lives > 0;) {
     console.log(`Level ${level + 1}, lives: ${lives}`);
@@ -348,4 +346,71 @@ async function runGame(plans, Display) {
   } else {
     console.log("Game over");
   }
+}
+
+// Your code here
+function runLevel(level, Display) {
+  let display = new Display(document.body, level);
+  let state = State.start(level);
+  let ending = 1;
+  let running = "yes";
+
+  return new Promise(resolve => {
+    function escHandler(event) {
+      if (event.key != "Escape") return;
+      event.preventDefault();
+      if (running == "no") {
+        running = "yes";
+        runAnimation(frame);
+      } else if (running == "yes") {
+        running = "pausing";
+      } else {
+        running = "yes";
+      }
+    }
+    
+    window.addEventListener("keydown", escHandler);
+    
+    let arrowKeys = trackKeys(["ArrowLeft", "ArrowRight", "ArrowUp"]);
+
+    function frame(time) {
+      if (running == "pausing") {
+        running = "no";
+        return false;
+      }
+
+      state = state.update(time, arrowKeys);
+      display.syncState(state);
+      if (state.status == "playing") {
+        return true;
+      } else if (ending > 0) {
+        ending -= time;
+        return true;
+      } else {
+        display.clear();
+        window.removeEventListener("keydown", escHandler);
+        arrowKeys.unregister();
+        resolve(state.status);
+        return false;
+      }
+    }
+    runAnimation(frame);
+  });
+}
+
+function trackKeys(keys) {
+  let down = Object.create(null);
+  function track(event) {
+    if (keys.includes(event.key)) {
+      down[event.key] = event.type == "keydown";
+      event.preventDefault();
+    }
+  }
+  window.addEventListener("keydown", track);
+  window.addEventListener("keyup", track);
+  down.unregister = () => {
+    window.removeEventListener("keydown", track);
+    window.removeEventListener("keyup", track);
+  };
+  return down;
 }
